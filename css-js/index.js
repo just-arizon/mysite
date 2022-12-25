473a39128cf2ac57f8c4560f0850048d59e33cc8
Arinze <calistusarinzechinweuba@gmail.com>
2022-12-25 23:07:08 +0100

    Add styles and js that toggles nav-bar on and off

--- css-js/index.js
+++ css-js/index.js
@@ -4,3 +4,12 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", () =>{
   loader.style.display = "none"
 });
+
+//  Hamburger
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("active")
+})
